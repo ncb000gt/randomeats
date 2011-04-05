b39ff392c72a74008a3f80653307243dde53b5f6
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-04-05 17:10:29 -0400

    Initial commit. Nothing outside of a standard rails app...move along.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randomeats::Application.config.secret_token = '9b6a02bccc528f611f3640b89d587f464b8c06191f082893f5d90bbec94160e5356e26a105183ac4ff5495dac5d71cde322933ee5582f64ce34be6aee0b2c4f5'
