96df44bc3a031bcb1e8bb9db5aa10e41e7309654
Nick Campbell <nicholas.j.campbell@gmail.com>
2011-04-06 17:11:51 -0400

    Randomizing eateries...just add some.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,12 @@ class HomeController < ApplicationController
   end
 
   def random
+    @t = params[:tag_ids] || nil 
+    if (@t != nil)
+      @joints = Joint.joins(:tags).where('tags.name' => @t.split(',')).find(:all)#.count(:all)
+    else
+      @joints = Joint.find(:all)#.count(:all)
+    end
+    @r = @joints[rand(@joints.length)].name
   end
 end
